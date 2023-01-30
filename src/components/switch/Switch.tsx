6e321dacea3f9dc05c3731646cd44796dd0325d0
xsuve <georgebaba99@yahoo.com>
2023-01-30 20:47:59 +0200

    Switch adjusted height.

--- src/components/switch/Switch.tsx
+++ src/components/switch/Switch.tsx
@@ -13,7 +13,7 @@ const Switch: FC<SwitchProps> = ({
 }) => {
   return (
     <div
-      className={`w-[32px] h-[12px] relative flex items-center rounded-lg cursor-pointer transition-all ease-linear ${disabled ? 'bg-gray-100' : (toggled ? 'bg-yellow-200' : 'bg-white')}`}
+      className={`w-[32px] h-[10px] relative flex items-center rounded-lg cursor-pointer transition-all ease-linear ${disabled ? 'bg-gray-100' : (toggled ? 'bg-yellow-200' : 'bg-white')}`}
       onClick={disabled ? null : onClick}
     >
       <div className={`w-[16px] h-[16px] rounded-full transition-all ease-linear transform ${disabled ? 'bg-gray-300' : (toggled ? 'bg-yellow-500 translate-x-full' : 'bg-gray-300 translate-x-0')}`}></div>
