eb0cf3c2ce066d15d4714a96b607bfa0399debdf
xsuve <georgebaba99@yahoo.com>
2023-02-22 22:35:03 +0200

    Remove commented input.

--- src/components/select/Select.tsx
+++ src/components/select/Select.tsx
@@ -37,25 +37,23 @@ const Select: FC<SelectProps> = ({
   return (
     <div className={`text-left ${className}`}>
       { label ? <Text type='label' color='dark' className='block mb-2'>{label}</Text> : null }
-      {/* <div className={`rounded h-[45px] border group ${errors && errors[name] ? 'border-red-400 hover:border-red-400' : 'border-slate-200 hover:border-slate-300'}`}> */}
-        <ReactSelect
-          ref={field.ref}
-          name={field.name}
-          placeholder={placeholder}
-          options={options}
-          required={required}
-          classNames={{
-            control: () => `!rounded !h-[45px] group !border !border-slate-200 !hover:border-slate-300 !shadow-none`,
-            placeholder: () => `!text-[13px] font-poppins font-normal !text-slate-400 !group-hover:text-slate-500`,
-            singleValue: () => `!text-[13px] font-poppins font-normal !text-slate-500`,
-            indicatorSeparator: () => `!bg-slate-200 !group-hover:bg-slate-300`,
-            indicatorsContainer: () => `!text-slate-200 !group-hover:text-slate-300`,
-            menu: () => `!rounded !shadow-none !border !border-slate-200`,
-            menuList: () => `!py-0`,
-            option: (state) => `!text-[13px] font-poppins font-normal ${state.isSelected ? '!text-white' : '!text-slate-500'}`
-          }}
-        />
-      {/* </div> */}
+      <ReactSelect
+        ref={field.ref}
+        name={field.name}
+        placeholder={placeholder}
+        options={options}
+        required={required}
+        classNames={{
+          control: () => `!rounded !h-[45px] group !border !border-slate-200 !hover:border-slate-300 !shadow-none`,
+          placeholder: () => `!text-[13px] font-poppins font-normal !text-slate-400 !group-hover:text-slate-500`,
+          singleValue: () => `!text-[13px] font-poppins font-normal !text-slate-500`,
+          indicatorSeparator: () => `!bg-slate-200 !group-hover:bg-slate-300`,
+          indicatorsContainer: () => `!text-slate-200 !group-hover:text-slate-300`,
+          menu: () => `!rounded !shadow-none !border !border-slate-200`,
+          menuList: () => `!py-0`,
+          option: (state) => `!text-[13px] font-poppins font-normal ${state.isSelected ? '!text-white' : '!text-slate-500'}`
+        }}
+      />
       { errors && errors[name]?.type === 'required' ? <Text type='paragraph' color='red' className='!text-xs mt-1'>{errors[name]?.message}</Text> : null }
       { errors && errors[name]?.type === 'pattern' ? <Text type='paragraph' color='red' className='!text-xs mt-1'>{errors[name]?.message}</Text> : null }
       { errors && errors[name]?.type === 'minLength' ? <Text type='paragraph' color='red' className='!text-xs mt-1'>{errors[name]?.message}</Text> : null }
