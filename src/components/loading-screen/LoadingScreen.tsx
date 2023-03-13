268d613685771ca82dc8a18fec29f4c3c7b0f16e
xsuve <georgebaba99@yahoo.com>
2023-03-13 14:12:21 +0200

    Loading screen and layout fixes.

--- src/components/loading-screen/LoadingScreen.tsx
+++ src/components/loading-screen/LoadingScreen.tsx
@@ -3,8 +3,10 @@ import { Logo } from '@/components/ui';
 
 const LoadingScreen: FC = () => {
   return (
-    <div className='h-[500px] w-full bg-creamy flex justify-center items-center'>
-      <Logo />
+    <div className={`bg-white h-[500px] p-4`}>
+      <div className={`h-full overflow-hidden z-20 relative p-4 bg-creamy rounded-2xl flex justify-center items-center`}>
+        <Logo />
+      </div>
     </div>
   );
 };
